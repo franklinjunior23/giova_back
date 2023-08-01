1a3b64fd4668893aae72be4706f3e7569027093a
Franx Junior <franklinjunior021118@gmail.com>
2023-08-01 14:23:49 -0500

    proyecto terminado byh

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -24,8 +24,8 @@ function Carrusel() {
               className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right"
               style={{
                 backgroundImage:
-                  'url("https://images.pexels.com/photos/3735641/pexels-photo-3735641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                backgroundPosition: "center", // Añadimos esta línea para centrar la imagen
+                  'url("https://images.pexels.com/photos/4064835/pexels-photo-4064835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+                backgroundPosition: "bottom", // Añadimos esta línea para centrar la imagen
                 backgroundSize: "cover", // Configuramos la imagen como "cover"
               }}
             >
@@ -73,13 +73,13 @@ function Carrusel() {
               className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right"
               style={{
                 backgroundImage:
-                  'url("https://images.pexels.com/photos/89910/pexels-photo-89910.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+                  'url("https://images.pexels.com/photos/667838/pexels-photo-667838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
               }}
             >
               <div className="container mx-auto">
                 <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
                 <p className="text-white font-semibold text-2xl my-4">
-                No vendemos ropa, vendemos armaduras para conquistar al mundo.
+                  ¡Descubre el lujo en cada detalle con nuestro producto, diseñado para realzar tu estilo!
                   </p>
                   <a
                     className="text-xl inline-block text-white no-underline border-b border-white leading-relaxed"
@@ -120,18 +120,18 @@ function Carrusel() {
               className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-bottom"
               style={{
                 backgroundImage:
-                  'url("https://images.pexels.com/photos/3762800/pexels-photo-3762800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+                  'url("https://images.pexels.com/photos/1005058/pexels-photo-1005058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
                   backgroundPosition: "center", // Añadimos esta línea para centrar la imagen
                   backgroundSize: "cover", // Configuramos la imagen como "cover"
               }}
             >
               <div className="container mx-auto">
                 <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
-                <p className="text-black font-semibold text-2xl my-4">
-                No podés comprar la felicidad, pero podés comprar ropa y es casi lo mismo.
+                <p className="text-white font-semibold text-2xl my-4">
+                  ¡Descubre el lujo en cada detalle con nuestro producto, diseñado para realzar tu estilo!
                   </p>
                   <a
-                    className="text-xl inline-block text-black  no-underline border-b border-black leading-relaxed"
+                    className="text-xl inline-block text-white no-underline border-b border-white leading-relaxed"
                     href="#"
                   >
                     view product
